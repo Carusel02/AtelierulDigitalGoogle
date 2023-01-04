9d148455f0e29eefca7b067eb22d6ac8b6356bb2
Marius <marinmariusdaniel02@gmail.com>
2023-01-04 23:04:35 +0200

    incercare achievement.
    Co-authored-by: Diana sg22dia@gmail.com

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Super super tare! ");
         System.out.println("Super super super tare! ");
         System.out.println("nu inteleg de ce nu merge");
+        System.out.println("incercare iar ");
     }
 }
