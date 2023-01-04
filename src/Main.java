84d096cca31337a9c3d886041a0e1c41f204698b
Marius <marinmariusdaniel02@gmail.com>
2023-01-04 22:24:43 +0200

    update. Co-authored-by: Diana sg22dia@gmail.com

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Diana e tare! ");
         System.out.println("Super tare! ");
+        System.out.println("Super super tare! ");
     }
 }
