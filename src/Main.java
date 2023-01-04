cf71c14cc85431ac10865eb03a29b5ceb80518aa
Marius <marinmariusdaniel02@gmail.com>
2023-01-04 22:43:36 +0200

    try unlock.
    Co-authored-by: sg22dia@gmail.com

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Super tare! ");
         System.out.println("Super super tare! ");
         System.out.println("Super super super tare! ");
+        System.out.println("nu inteleg de ce nu merge");
     }
 }
